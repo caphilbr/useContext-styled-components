34d0f4cbc8fa6fcc174df919577ecdcc1a39243b
caphilbr <caphilbr@hotmail.com>
2024-07-17 15:50:31 -0400

    full example

---
+++ src/SmallButton.js
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+const SmallButton = styled.span`
+  background-color: lightgrey;
+  color: black;
+  padding: 7px 15px;
+  border-radius: 0.25rem;
+  text-align: center;
+  display: inline-block;
+  font-size: 10px;
+  margin: 3px 2px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: grey;
+  }
+`
+
+export default SmallButton
---
+++ src/SmallButtonContainer.js
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+const SmallButtonContainer = styled.span`
+  padding: 7px 15px;
+  text-align: center;
+  display: block;
+  margin: 3px 2px;
+`
+
+export default SmallButtonContainer
